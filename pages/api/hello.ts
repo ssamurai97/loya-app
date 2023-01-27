4b7ade90420fb8d400c1f996cb91def72ec7e70a
kuchlong kuchlong <kuchlong@gmail.com>
2023-01-27 16:10:44 -0600

    edited hello.ts file

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export const config = {
+	runtime: 'experimental-edge'
+}
+
+
 type Data = {
   name: string
 }
+
+
 
 export default function handler(
   req: NextApiRequest,
